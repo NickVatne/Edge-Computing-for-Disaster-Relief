df1dd2c2dfd133f8de3d368c54f55af3e42434f1
Nicolai Vatne <nicovatne@gmail.com>
2022-03-11 21:14:03 +0100

    ssl/tls mosquitto

--- data-simulator/src/beq_controller/src/beq_controller.py
+++ data-simulator/src/beq_controller/src/beq_controller.py
@@ -9,11 +9,11 @@ broker = "192.168.0.241"
 port = 1883
 
 
-def temp_controller(broker, port):
+def bequeruel_measurement(broker, port):
     unique_id = uuid.uuid4()
-    topic = "home/temperature/"
+    topic = "home/bequerel/"
     fault = "test"
-    deviceID = "Temp-Controller-RPI" + unique_id.__str__()
+    deviceID = "Bequeruel-Controller-RPI" + unique_id.__str__()
     client = mqtt.Client(deviceID)
 
     while True:
@@ -22,9 +22,9 @@ def temp_controller(broker, port):
         data["timeStamp"] = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
 
         if fault == "1":
-            data["temperature"] = round(random.uniform(30.0, 60.0), 1)
+            data["bequerel"] = round(random.uniform(30.0, 10000.0), 1)
         else:
-            data["temperature"] = round(random.uniform(30.0, 55.0), 1)
+            data["bequerel"] = round(random.uniform(30.0, 10000.0), 1)
 
         payload = json.dumps(data, ensure_ascii=False)
         print(payload)
@@ -34,4 +34,4 @@ def temp_controller(broker, port):
 
 
 if __name__ == '__main__':
-    temp_controller(broker, port)
+    bequeruel_measurement(broker, port)
--- data-simulator/src/fan_controller/src/fan_controller.py
+++ data-simulator/src/fan_controller/src/fan_controller.py
@@ -9,11 +9,11 @@ broker = "192.168.0.241"
 port = 1883
 
 
-def temp_controller(broker, port):
+def fan_controller(broker, port):
     unique_id = uuid.uuid4()
-    topic = "home/temperature/"
+    topic = "home/fan/"
     fault = "test"
-    deviceID = "Temp-Controller-RPI" + unique_id.__str__()
+    deviceID = "Fan-Controller-RPI" + unique_id.__str__()
     client = mqtt.Client(deviceID)
 
     while True:
@@ -22,9 +22,13 @@ def temp_controller(broker, port):
         data["timeStamp"] = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
 
         if fault == "1":
-            data["temperature"] = round(random.uniform(30.0, 60.0), 1)
+            data["rpm"] = round(random.uniform(300, 900), 1)
+            data["temp"] = round(random.uniform(30.0, 60.0), 1)
+            data["sound"] = round(random.uniform(45, 60), 1)
         else:
-            data["temperature"] = round(random.uniform(30.0, 55.0), 1)
+            data["rpm"] = round(random.uniform(500, 1000), 1)
+            data["temp"] = round(random.uniform(30.0, 55.0), 1)
+            data["sound"] = round(random.uniform(45, 70), 1)
 
         payload = json.dumps(data, ensure_ascii=False)
         print(payload)
@@ -34,4 +38,4 @@ def temp_controller(broker, port):
 
 
 if __name__ == '__main__':
-    temp_controller(broker, port)
+    fan_controller(broker, port)
