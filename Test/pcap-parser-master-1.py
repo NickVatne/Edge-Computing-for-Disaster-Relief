f474d531728cd7cf06e0375e86352fdd7603b7fc
Nicolai Vatne <nicovatne@gmail.com>
2022-04-29 08:56:54 +0200

    revised testing using wondershaper

--- Test/pcap-parser-master-1.py
+++ Test/pcap-parser-master-1.py
@@ -27,34 +27,36 @@ def tcp_flags(file_name):
 
 def find_average_cnr():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/1cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/2cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/3cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/4cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/5cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/6cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/7cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/8cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/9cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/10cnr_network_1%loss_1%_5%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-10-new-old/1cnr-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-10-new-old/2cnr-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-10-new-old/3cnr-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-10-new-old/4cnr-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-10-new-old/5cnr-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-10-new-old/6cnr-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-10-new-old/7cnr-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-10-new-old/8cnr-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-10-new-old/9cnr-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-10-new-old/10cnr-1gemodel.pcap')
     cnr_arr = [capture_one, capture_two, capture_three, capture_four,
                capture_five, capture_six, capture_seven, capture_eight, capture_nine, capture_ten]
-    print(cnr_arr)
+    cnr_arr.remove(min(cnr_arr))
+    cnr_arr.remove(min(cnr_arr))
+    cnr_arr.remove(min(cnr_arr))
     return cnr_arr
 
 
 def find_average_midband():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-hadr-run-second/1midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-hadr-run-second/2midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-hadr-run-second/3midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-hadr-run-second/4midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-hadr-run-second/4midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-hadr-run-second/6midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-hadr-run-second/7midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-hadr-run-second/8midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-hadr-run-second/9midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-hadr-run-second/10midband_1%_loss_1%_5%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-10-new-old/1mid-band-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-10-new-old/2mid-band-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-10-new-old/3mid-band-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-10-new-old/4mid-band-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-10-new-old/4mid-band-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-10-new-old/6mid-band-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-10-new-old/7mid-band-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-10-new-old/8mid-band-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-10-new-old/9mid-band-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-10-new-old/10mid-band-1gemodel.pcap')
     midband_arr = [capture_one, capture_two, capture_three, capture_four,
                    capture_five, capture_six, capture_seven, capture_eight, capture_nine, capture_ten]
     return midband_arr
@@ -62,52 +64,63 @@ def find_average_midband():
 
 def find_average_nato():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/1NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/2NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/3NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/4NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/5NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/6NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/7NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/8NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/9NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/10NATO_Narrowband_waveform_1%loss_1%_5%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-10-new-old/1NATO-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-10-new-old/2NATO-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-10-new-old/3NATO-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-10-new-old/4NATO-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-10-new-old/5NATO-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-10-new-old/6NATO-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-10-new-old/7NATO-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-10-new-old/8NATO-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-10-new-old/8NATO-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-10-new-old/10NATO-1gemodel.pcap')
     nato_arr = [capture_one, capture_two, capture_three, capture_four,
                 capture_five, capture_six, capture_seven, capture_eight, capture_nine, capture_ten]
     nato_arr.remove(min(nato_arr))
+    nato_arr.remove(max(nato_arr))
+    nato_arr.remove(max(nato_arr))
     nato_arr.remove(min(nato_arr))
-    nato_arr.remove(min(nato_arr))
-    print(nato_arr)
     return nato_arr
 
 
 def find_average_tactical():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/1tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/2tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/3tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/4tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/5tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/6tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/7tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/8tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/9tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-results-10runs/hadr-master-eth0/10tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-10-new-old/1tactical-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-10-new-old/2tactical-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-10-new-old/3tactical-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-10-new-old/4tactical-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-10-new-old/5tactical-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-10-new-old/6tactical-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-10-new-old/7tactical-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-10-new-old/8tactical-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-10-new-old/9tactical-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-10-new-old/10tactical-1gemodel.pcap')
     tactical_arr = [capture_one, capture_two, capture_three, capture_four, capture_five, capture_six,
                     capture_seven, capture_eight, capture_nine, capture_ten]
     tactical_arr.remove(min(tactical_arr))
-    tactical_arr.remove(max(tactical_arr))
     return tactical_arr
 
 
+
+def test_find():
+    nato_capture = number_of_lost_segments('27-04-10-new-old/run-2/1NATO-1gemodel.pcap')
+    cnr_capture = number_of_lost_segments('27-04-10-new-old/run-2/1cnr-1gemodel.pcap')
+    tactical_capture = number_of_lost_segments('27-04-10-new-old/run-2/1tactical-1gemodel.pcap')
+    midband = number_of_lost_segments('27-04-10-new-old/run-2/1mid-band-1gemodel.pcap')
+    print('CNR', cnr_capture, 'Nato', nato_capture, 'Tactical', tactical_capture, 'midband', midband)
+
+
 def box_graph(cnr, midband, nato, tactical_arr):
-    dict = {'CNR': cnr, 'Mid-Band 5G': midband, 'Nato Narrow': nato, 'Tactical Network': tactical_arr }
+    dict = {'CNR': nato, 'Mid-Band 5G': midband, 'NBWF': cnr, 'Tactical Network': tactical_arr}
+    print(dict)
     fig, ax = plt.subplots()
 
     plt.boxplot(dict.values())
     ax.set_xticklabels(dict.keys())
-    ax.set_title("K3s Master node 1% loss")
-    matplotlib.pyplot.savefig("master-node-1loss.svg", format="svg")
+    ax.set_title("K3s Master node (10% Loss, 5% Gilbert Elliott)", fontsize=14)
+    plt.xlabel('Emulated Networks', fontsize=14)
+    plt.ylabel('Packet loss over 10000 packets', fontsize=14)
+    matplotlib.pyplot.savefig("master-node-stable-10loss-run2.svg", format="svg")
     # show plot
     plt.show()
 
--- Test/pcap-parser.py
+++ Test/pcap-parser.py
@@ -27,90 +27,89 @@ def tcp_flags(file_name):
 
 def find_average_cnr():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/1cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/2cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/3cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/4cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/5cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/6cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/7cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/8cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/9cnr_network_1%loss_1%_5%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/10cnr_network_1%loss_1%_5%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-1%-broker/1cnr-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-1%-broker/2cnr-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-1%-broker/3cnr-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-1%-broker/4cnr-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-1%-broker/5cnr-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-1%-broker/6cnr-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-1%-broker/7cnr-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-1%-broker/8cnr-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-1%-broker/9cnr-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-1%-broker/10cnr-1gemodel.pcap')
     cnr_arr = [capture_one, capture_two, capture_three, capture_four,
                capture_five, capture_six, capture_seven, capture_eight, capture_nine, capture_ten]
+    cnr_arr.remove(min(cnr_arr))
+    cnr_arr.remove(min(cnr_arr))
+    cnr_arr.remove(min(cnr_arr))
     return cnr_arr
 
 
 def find_average_midband():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/1midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/2midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/3midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/4midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/9midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/6midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/7midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/8midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/9midband_1%_loss_1%_5%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/10midband_1%_loss_1%_5%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-1%-broker/1mid-band-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-1%-broker/3mid-band-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-1%-broker/3mid-band-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-1%-broker/4mid-band-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-1%-broker/5mid-band-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-1%-broker/6mid-band-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-1%-broker/7mid-band-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-1%-broker/8mid-band-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-1%-broker/9mid-band-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-1%-broker/10mid-band-1gemodel.pcap')
     midband_arr = [capture_one, capture_two, capture_three, capture_four,
                    capture_five, capture_six, capture_seven, capture_eight, capture_nine, capture_ten]
+    midband_arr.remove(max(midband_arr))
+    midband_arr.remove(max(midband_arr))
     midband_arr.remove(max(midband_arr))
     return midband_arr
 
 
 def find_average_nato():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/1NATO_Narrowband_waveform_1%loss_1%_5'
-                                          '%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/2NATO_Narrowband_waveform_1%loss_1%_5'
-                                          '%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/3NATO_Narrowband_waveform_1%loss_1%_5'
-                                            '%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/4NATO_Narrowband_waveform_1%loss_1%_5'
-                                           '%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/5NATO_Narrowband_waveform_1%loss_1%_5'
-                                           '%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/6NATO_Narrowband_waveform_1%loss_1%_5'
-                                          '%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/7NATO_Narrowband_waveform_1%loss_1%_5'
-                                            '%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/8NATO_Narrowband_waveform_1%loss_1%_5'
-                                            '%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/9NATO_Narrowband_waveform_1%loss_1%_5'
-                                           '%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/10NATO_Narrowband_waveform_1%loss_1%_5'
-                                          '%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-1%-broker/1NATO-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-1%-broker/2NATO-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-1%-broker/3NATO-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-1%-broker/4NATO-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-1%-broker/5NATO-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-1%-broker/6NATO-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-1%-broker/7NATO-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-1%-broker/8NATO-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-1%-broker/9NATO-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-1%-broker/10NATO-1gemodel.pcap')
     nato_arr = [capture_one, capture_two, capture_three, capture_four,
                 capture_five, capture_six, capture_seven, capture_eight, capture_nine, capture_ten]
+    nato_arr.remove(min(nato_arr))
+    nato_arr.remove(min(nato_arr))
+    nato_arr.remove(min(nato_arr))
+    nato_arr.remove(max(nato_arr))
     return nato_arr
 
 
 def find_average_tactical():
     # Total of 5 runs, get all and find average number of runs
-    capture_one = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/1tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_two = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/2tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_three = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/3tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_four = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/4tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_five = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/5tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_six = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/6tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_seven = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/7tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_eight = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/8tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_nine = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/9tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
-    capture_ten = number_of_lost_segments('eth-results-10runs/hadr-broker-eth/10tactical_broadband_1%loss_1%_5%_10%_10%.pcap')
+    capture_one = number_of_lost_segments('27-04-1%-broker/1tactical-1gemodel.pcap')
+    capture_two = number_of_lost_segments('27-04-1%-broker/2tactical-1gemodel.pcap')
+    capture_three = number_of_lost_segments('27-04-1%-broker/3tactical-1gemodel.pcap')
+    capture_four = number_of_lost_segments('27-04-1%-broker/4tactical-1gemodel.pcap')
+    capture_five = number_of_lost_segments('27-04-1%-broker/5tactical-1gemodel.pcap')
+    capture_six = number_of_lost_segments('27-04-1%-broker/6tactical-1gemodel.pcap')
+    capture_seven = number_of_lost_segments('27-04-1%-broker/7tactical-1gemodel.pcap')
+    capture_eight = number_of_lost_segments('27-04-1%-broker/8tactical-1gemodel.pcap')
+    capture_nine = number_of_lost_segments('27-04-1%-broker/9tactical-1gemodel.pcap')
+    capture_ten = number_of_lost_segments('27-04-1%-broker/10tactical-1gemodel.pcap')
     tactical_arr = [capture_one, capture_two, capture_three, capture_four, capture_five, capture_six,
                     capture_seven, capture_eight, capture_nine, capture_ten]
     print(tactical_arr)
-    tactical_arr.remove(min(tactical_arr))
-    tactical_arr.remove(min(tactical_arr))
+    tactical_arr.remove(max(tactical_arr))
+    tactical_arr.remove(max(tactical_arr))
     return tactical_arr
 
 
 def box_graph(cnr, midband, nato, tactical_arr):
-    dict = {'CNR': cnr, 'Mid-Band 5G': midband, 'Nato Narrow': nato, 'Tactical Network': tactical_arr }
+    dict = {'CNR': cnr, 'Mid-Band 5G': midband, 'Nato Narrow': nato, 'Tactical Network': tactical_arr}
+    print(dict)
     fig, ax = plt.subplots()
-
     plt.boxplot(dict.values())
     ax.set_xticklabels(dict.keys())
     ax.set_title("K3s Broker 1% loss")
